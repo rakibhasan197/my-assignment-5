5462c71fbf0b2a0266d6f7253a9782219bf16e5b
rakibhasan197 <alhasanmdrakib062@gmail.com>
2026-03-06 23:43:36 +0600

    i created login page successfully

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hello js')
+document.getElementById('login').addEventListener('click', function(){
+  const userName = document.getElementById('user-name')
+  const userInput = userName.value;
+  if(userInput !== 'admin'){
+    alert('invalid')
+    return
+  }
+
+  const inputPassword = document.getElementById('input-password');
+  const inputValue = inputPassword.value;
+  if(inputValue !== 'admin123'){
+    alert('invalid password')
+    return
+  }
+  window.location.assign('/homepage.html')
+  
+})
